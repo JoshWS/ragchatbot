faa33a31b4216b435ab7ab6728390b05b090647f
joshws <joshxteen@gmail.com>
2024-08-25 11:52:13 +0200

    cleanup

--- create_database.py
+++ create_database.py
@@ -8,25 +8,30 @@ import os
 import shutil
 
 import nltk
-nltk.download('punkt_tab')
-nltk.download('averaged_perceptron_tagger_eng')
+
+nltk.download("punkt_tab")
+nltk.download("averaged_perceptron_tagger_eng")
 
 CHROMA_PATH = "chroma"
 DATA_PATH = "data/books"
 
+
 def main():
     generate_data_store()
+
 
 def generate_data_store():
     documents = load_documents()
     chunks = split_text(documents)
     save_to_chroma(chunks)
 
+
 # Load all documents from datapath directory
 def load_documents():
     loader = DirectoryLoader(DATA_PATH, glob="*.md")
     documents = loader.load()
     return documents
+
 
 # Split text into manageable chunks
 def split_text(documents: list[Document]):
@@ -46,15 +51,19 @@ def split_text(documents: list[Document]):
 
     return chunks
 
+
 def save_to_chroma(chunks: list[Document]):
     # Clear out the database first.
     if os.path.exists(CHROMA_PATH):
         shutil.rmtree(CHROMA_PATH)
 
     # Create a new DB from the documents.
-    db = Chroma.from_documents(chunks, OpenAIEmbeddings(), persist_directory=CHROMA_PATH)
+    db = Chroma.from_documents(
+        chunks, OpenAIEmbeddings(), persist_directory=CHROMA_PATH
+    )
     db.persist()
     print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}")
+
 
 if __name__ == "__main__":
     main()
